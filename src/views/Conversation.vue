a50bdc6da39e9ab714753da2380a608c6ae54613
sven <sven2093@arcor.de>
2021-09-14 19:32:09 +0200

    removed some unused stuff

--- src/views/Conversation.vue
+++ src/views/Conversation.vue
@@ -1,33 +1,5 @@
 <template>
     <div v-for="(item) in conversationMessageList.filter((element, index) => index >= pageOffset && index < (pageOffset + limit))" v-bind:key="item.id" class="p-5"><Message :message="item" :userId="userId"></Message></div>
-
-  <!--
-  <div id="conversation">
-    <div id="conversation-header" class="text-left fixed w-full bg-white">
-      <div class="flex justify-between font-bold text-xl">{{ displayName }}</div>
-    </div>
-    <div id="conversation-content">
-      <div v-for="(item) in conversationMessageList.filter((element, index) => index >= pageOffset && index < (pageOffset + limit))" v-bind:key="item.id" class="p-5"><Message :message="item" :userId="userId"></Message></div>
-    </div>
-  </div>
-  -->
-  <!--<div v-if="conversation">
-    <div>Conversation with {{ id }}</div>-->
-    <!--<div v-for="(item) in conversationMessageList" v-bind:key="item.id" class="p-5"><Message :message="item" :userId="userId"></Message></div>-->
-    <!--<div v-for="(item) in conversationMessageList.filter((element, index) => index >= pageOffset && index < (pageOffset + limit))" v-bind:key="item.id" class="p-5"><Message :message="item" :userId="userId"></Message></div>-->
-  <!--</div>-->
-  <!--
-  <div v-if="conversation" class="text-center">
-    Conversation with {{ id }}
-    <div v-if="Math.ceil(conversationSize / limit) > 0">
-      <button v-if="page > 1" class="pr-5" v-on:click="page -= 1">&#60;</button>
-      <button v-if="page < Math.ceil(conversationSize / limit)" class="pl-5" v-on:click="page += 1">&#62;</button>
-    </div>
-    <div id="conversation">
-      <div v-for="(item) in conversationMessageList.filter((element, index) => index >= pageOffset && index < (pageOffset + limit))" v-bind:key="item.id" class="p-5"><Message :message="item" :userId="userId"></Message></div>
-    </div>
-  </div>
-  -->
 </template>
 
 <script lang="ts">
@@ -56,11 +28,9 @@ export default defineComponent({
     pageOffset() { return (this.page - 1) * this.limit; },
 
     id() { return this.$route.params.id; },
-    displayName() { return this.$store.state.conversations.find(element => element.id == this.$route.params.id)?.displayName },
-    conversation(): Conversation | undefined { return this.$store.state.conversations.find(element => element.id == this.$route.params.id); },
-    conversationSize(): number { return this.conversation?.MessageList.length || 1 },
-    conversationMessageList(): MessageType[] { return this.conversation?.MessageList || [] },
     userId() { return this.$store.state.userId; },
+    conversation(): Conversation | undefined { return this.$store.state.conversations.find(element => element.id == this.$route.params.id); },
+    conversationMessageList(): MessageType[] { return this.conversation?.MessageList || [] },
   },
   mounted() {
     this.$store.commit('parseConversation', this.$route.params.id)
